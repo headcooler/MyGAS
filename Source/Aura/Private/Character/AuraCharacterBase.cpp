b98cfc0495cb067c5f350f7816144763a94cc0bd
headcooler <headcooler@naver.com>
2024-04-13 01:04:06 +0900

    Git Ignore 수정, AuraCharacterBase 클래스를 Character폴더로 이동

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,14 @@
+// Copyright Headcooler
+
+#include "Character/AuraCharacterBase.h"
+
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+}
