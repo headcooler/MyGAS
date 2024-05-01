4ffd79f72ea3aaf4e07990a656f32867d8db8533
headcooler <headcooler@naver.com>
2024-05-01 23:58:40 +0900

    127. Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,10 @@
+// Copyright Headcooler
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
